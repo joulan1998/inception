a90dca8c614ecb0ee8cbb3568a40aea8a6f22fdf
joulan1998 <elgarraoui.ayoub98@gmail.com>
2025-12-28 15:01:12 +0100

    before last push to my own repo

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-# WP_PATH="/var/www/html"
 WP_PATH="/var/www/wordpress"
 
 mkdir -p /run/php
@@ -36,35 +35,3 @@ if [ ! -f "$WP_PATH/wp-config.php" ]; then
 fi
 
 exec php-fpm
-
-# #!/bin/bash
-
-# # Download and install WordPress
-# wget https://wordpress.org/latest.tar.gz -P /var/www/
-# # tar -xzvf /var/www/latest.tar.gz -C /var/www/
-# tar -xzvf /var/www/latest.tar.gz --directory /var/www/
-# cp -f  /var/www/wordpress /var/www/wordpress
-# # mv -f /var/www/wordpress /var/www/wordpress
-
-# # Create a symbolic link for WordPress to be in the root of the volume
-# ln -s /var/www/wordpress /var/www/html
-
-# # Clean up the downloaded file
-# rm /var/www/latest.tar.gz
-
-# # Navigate to the WordPress directory
-# cd /var/www/wordpress
-
-# # Configure WordPress
-# wp config create --allow-root --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=mariadb --path='/var/www/wordpress'
-
-# # Install WordPress
-# wp core install --allow-root --url=${DOMAIN_NAME} --title="${WP_TITLE}" --admin_user=${WP_USER} --admin_password=${WP_PASSWORD} --admin_email=${WP_EMAIL}
-
-# # Create a second user
-# wp user create second_user second_user@example.com --allow-root --path='/var/www/wordpress'
-
-# # Set permissions
-# chown -R www-data:www-data /var/www/wordpress
-# chmod -R 755 /var/www/wordpress
-# mkdir -p /run/php #its mine for docker logs word
